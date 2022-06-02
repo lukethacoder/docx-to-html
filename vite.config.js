e488b819f7fac5154868f10aeac3c8dad8db7538
Luke Secomb <luke.secomb@live.com>
2022-06-02 13:55:48 +1000

    🐛 fixing build imports

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 })
